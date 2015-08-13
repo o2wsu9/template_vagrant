0955d0490b73e6e64600b4f6435e2b222274df79
o2wsu9 <o2wsu9@gmail.com>
2015-08-13 10:47:09 +0900

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 if ! [ `which ansible` ]; then
-  yum localinstall http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm  
-  yum -y install ansible libselinux-python
+    rpm -Uvh http://ftp.riken.jp/Linux/fedora/epel/6/i386/epel-release-6-8.noarch.rpm
+    rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+    rpm -Uvh http://dl.iuscommunity.org/pub/ius/stable/CentOS/6/x86_64/ius-release-1.0-11.ius.centos6.noarch.rpm
+    yum -y install ansible libselinux-python
 fi
 
 ansible-playbook -i /vagrant/ansible/hosts /vagrant/ansible/site.yml
+
